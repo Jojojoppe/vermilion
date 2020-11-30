97a89833cbb2b161f79840c72aa4301445b652d7
Joppe Blondel <joppe@blondel.nl>
2020-11-30 20:17:13 +0100

    Edited CMakeLists files for windows
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,12 +1,13 @@
 #include <vermilion/vermilion.hpp>
 #include <vermilion/core/instance.hpp>
+#include <vermilion/window/window.hpp>
+
+#include <memory>
 
 int main(int argc, char ** argv){
 
-	Vermilion::Core::Instance vmInstance;
-
-	extern void vermilion_window();
-	vermilion_window();
+	auto vmInstance = std::make_shared<Vermilion::Core::Instance>();
+	auto vmWindow = std::make_shared<Vermilion::Window::Window>(vmInstance);
 
 	return 0;
 }
